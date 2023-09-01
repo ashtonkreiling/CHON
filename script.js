bb19cdf3e02c9c77c207b88b6173e3d45ef19f16
ashtonkreiling <ashtonkreiling@gmail.com>
2023-08-31 20:37:55 -0600

    Make script.js less level specific

--- script.js
+++ script.js
@@ -11,9 +11,6 @@ const CANVAS_HEIGHT = canvas.height = 600;
 const ATOM_HEIGHT = 200;
 const ATOM_WIDTH = 200;
 
-const oxygen = levelOne[0];
-const carbon = levelOne[1];
-
 let i = 0;
 let isPaused = false; // Flag to track if the animation is paused
 let originalImageData; // To store the original canvas content
@@ -23,8 +20,20 @@ function animate(){
     if (!isPaused) {
         i++;
         context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-        context.drawImage(oxygen.image, 0, 0, ATOM_WIDTH, ATOM_HEIGHT, oxygen.position[0], oxygen.position[1], 200, 200);
-        context.drawImage(carbon.image, 0, 0, ATOM_WIDTH, ATOM_HEIGHT, carbon.position[0], carbon.position[1], 200, 200);
+        for (let i = 0; i < levelOne.length; i++) {
+          let currentParticle = levelOne[i];
+          context.drawImage(
+            currentParticle.image, 
+            0, 
+            0, 
+            ATOM_WIDTH, 
+            ATOM_HEIGHT, 
+            currentParticle.position[0],
+            currentParticle.position[1],
+            200,
+            200
+          );
+        }
         if (i % 1 == 0) {
             updateScene(context);
         }
